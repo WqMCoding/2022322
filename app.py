1e25909161a72232bd51970974b5ed27af0f891f
WqMCoding <98138427+WqMCoding@users.noreply.github.com>
2022-03-22 21:34:50 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[58]:
+
+
+from flask import Flask,request,render_template
+
+
+# In[59]:
+
+
+app = Flask(__name__)
+
+
+# In[60]:
+
+
+from textblob import TextBlob
+from transformers import pipeline
+
+classifier = pipeline('sentiment-analysis', "mrm8488/bert-small-finetuned-squadv2")
+
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        text = request.form.get("text")
+        print(text)
+        r1 = TextBlob(text).sentiment
+        r2 = classifier(text)
+        return(render_template("index.html",result1=r1,result2=r2))
+    else:
+        return(render_template("index.html",result1="2",result2="2"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[52]:
+
+
+pip install transformers -U
+
